c771f17113cae04a69f5c38d265e58f2971b5cb7
calidor <victor.calidor@gmail.com>
2021-10-09 20:54:45 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,59 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function isItUndefined(funcionario, key) {
+  const emp = data.employees.find((employee) => employee[key] === funcionario.name);
+  if (emp === undefined && key === 'lastName') {
+    throw new Error('Informações inválidas');
+  }
+  return emp;
 }
+
+// Pegar funcionario
+function getFuncionario(funcionario) {
+  if (funcionario.name !== undefined) {
+    const func1 = isItUndefined(funcionario, 'firstName');
+
+    if (func1 === undefined) {
+      const func2 = isItUndefined(funcionario, 'lastName');
+      return func2;
+    }
+    return func1;
+  }
+
+  const func3 = data.employees.find((employee) => employee.id === funcionario.id);
+
+  if (func3 === undefined) {
+    throw new Error('Informações inválidas');
+  } return func3;
+}
+
+// Pegar especies dos funcionarios
+function getEmployeesSpecies(funcionario) {
+  const colaborators = funcionario.responsibleFor.reduce((col1, col2) => `${col1}, ${col2}`);
+  return species.filter((specie) => colaborators.includes(specie.id));
+}
+function getEmployeesCoverage(employee) {
+  if (employee === undefined) {
+    const empCover = data.employees.map((funcionario) => {
+      const especies = getEmployeesSpecies(funcionario);
+      return {
+        id: funcionario.id,
+        fullName: `${funcionario.firstName} ${funcionario.lastName}`,
+        species: especies.map((specie) => specie.name),
+        locations: especies.map((specie) => specie.location),
+      };
+    }); return empCover;
+  }
+  const funcionario = getFuncionario(employee);
+  const especies = getEmployeesSpecies(funcionario);
+  return {
+    id: funcionario.id,
+    fullName: `${funcionario.firstName} ${funcionario.lastName}`,
+    species: especies.map((specie) => specie.name),
+    locations: especies.map((specie) => specie.location) };
+}
+
+/// console.log(getEmployeesCoverage({name: 'Strauss'}));
 
 module.exports = getEmployeesCoverage;
