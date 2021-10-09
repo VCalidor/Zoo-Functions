bc7d518e6f26a385e565bcfb2c34049fd42f2872
calidor <victor.calidor@gmail.com>
2021-10-08 22:43:04 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,16 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const caseFirstName = data.employees.filter((employee) => employee.firstName === employeeName)[0];
+  const caseLastName = data.employees.filter((employee) => employee.lastName === employeeName)[0];
+
+  if (typeof caseFirstName === 'object') {
+    return caseFirstName;
+  } return caseLastName;
 }
 
 module.exports = getEmployeeByName;
